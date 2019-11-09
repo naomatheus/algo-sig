838887124f3fbd4e0a711a68b25952f16b534775
matthew Robinson <matthewrobinson@Esfirs-iPhone.attlocal.net>
2019-11-09 12:59:58 -0600

    added 2 solutions for dedup.py

--- dedup.py
+++ dedup.py
@@ -1,8 +1,10 @@
+# Solution 1
 nums = [1,2,2,3,6,6,7]
+nums1 = [1,2,2,3]
 
 def remove_duplicates(numbers):
 	i = 0
-	
+
 	while i < len(numbers)-1:
 
 		if numbers[i] == numbers[i+1]: 
@@ -12,8 +14,26 @@ def remove_duplicates(numbers):
 			i += 1
 	return len(numbers)
 
-unq_nums_len = remove_duplicates(nums)
-
-
+unq_nums_len = remove_duplicates(nums1)
 
 print(unq_nums_len,nums)
+
+# Solution 2
+
+def no_duplicates(nums):
+
+	if len(nums) == 0:
+		return 0
+
+	i = 0
+	for j in range(len(nums)):
+		if nums[j] != nums[i]:
+		# if unequal, collect unique values at the front of the list, increment both
+			i += 1
+			nums[i] = nums[j]
+		# else i stays the same, increment j
+
+	return i + 1
+
+unq_nums_2 = no_duplicates(nums1)
+print(unq_nums_2)
--- readme.md
+++ readme.md
@@ -2,5 +2,10 @@
 #### This repository contains select problems and solutions from Chicago Python Algorithm Special Interest Group, a monthly meetup in Chicago, IL focused on all things Python and all things algorithm! 
 
 - Dedup.py
-   - Given a list of integers, remove duplicate numbers without creating a new list.
+   - Given a sorted list of integers, remove duplicate numbers without creating a new list.
    - Return the length of the de-duplicated list.
+   - 1st Solution uses pop() for O(n^2) complexity
+   - 2nd solutions tracks two indices for O(n) complexity
+
+- Game of Life
+   - 
