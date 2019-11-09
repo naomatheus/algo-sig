f0677ab3c5f1bcd05647bccdcf748ac27bac2b12
matthew Robinson <matthewrobinson@matthews-MacBook-Air-3.local>
2019-11-09 17:17:21 -0600

    completed more efficient sort

--- mock.py
+++ mock.py
@@ -2,8 +2,7 @@
 
 # 
 ## 
-list_1 = [1,3,4,5,6]
-list_2 = [2,3,4,7,8,9,9]
+
 
 def merge_lists(nums1, nums2):
 
@@ -13,11 +12,39 @@ def merge_lists(nums1, nums2):
 
 	return sorted(nums1)
 
-print(merge_lists(list_1,list_2))
+# print(merge_lists(list_1,list_2))
 
 # 0(m*log(n)) complexity where m is the number of items in the first list, and n is the number of items in the second merged list 
 
 ## More efficient pseudo code 
+list_1 = [1,3,4,5,6]
+list_2 = [2,3,4,7,8,9,9,10,11,13,15]
+
+def combine_lists(n,m):
+	sorted_list = []
+	i = 0
+	j = 0
+	while i < len(n) and j < len(m):
+		if n[i] <= m[j]:
+			sorted_list.append(n[i])
+			i+=1
+		elif m[j] <= n[i]:
+				sorted_list.append(m[j])
+				j+=1
+
+		while i < len(n):
+			sorted_list.append(n[i])
+			i+=1
+
+		while j < len(m):
+			sorted_list.append(m[j])
+			j+=1
+	
+	return sorted_list
+
+print(combine_lists(list_1,list_2))
+
+
 
 # while i < list1.len && j < list2.len
 # if (list1[i] <= list2[j])
